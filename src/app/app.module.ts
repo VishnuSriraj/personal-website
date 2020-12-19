37609714b15f24031859682c5be77f583e51f195
VishnuSriraj <vishnu.sriraj98@gmail.com>
2020-12-18 19:41:53 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { IntroComponent } from './intro/intro.component';
+import { KnowmeComponent } from './knowme/knowme.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkillsComponent,
+    IntroComponent,
+    KnowmeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
